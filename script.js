8e9edec602872f2662e2d32f7303aae2c9e12b23
InnovateBham_5 <InnovateBham_5@UABiLab-5s-iMac.local>
2017-06-27 16:54:20 -0500

    complete typinggame

--- script.js
+++ script.js
@@ -1,85 +1,90 @@
-var sentence = ['ten ate neite ate nee enet ite ate inet ent eate',
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+var keyPress;
+var b = -1;
+var start = new Date();
+var j = 0;
+var sentLength = sentences[j].length;
+var numMove = 0;
+var numOfWords = 54;
+var wrong = $('<span />').attr({'class':'glyphicon glyphicon-remove', 'aria-hidden':'true' });
+var numWrong = 0;
+var right = $('<span />').attr({'class':'glyphicon glyphicon-ok', 'aria-hidden':'true' });
+var lower = $('#keyboard-lower-container');
+var upper = $('#keyboard-upper-container')
 
-'Too ato too nOt enot one totA not anot tOO aNot',
-
-'oat itain oat tain nate eate tea anne inant nean',
-
-'itant eate anot eat nato inate eat anot tain eat', 
-
-'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-var sentenceCounter = 0;
-    letterCounter = 0;
-    currentSentence = '';
-
-$(document).ready(function () {
-    $("#keyboard-upper-container").hide();
+$(document).ready(function() {
+    upper.hide();
     
-$(document).on('keyup keydown', function (e) {
-        if (e.shiftKey) {
-            $("#keyboard-lower-container").hide();
-            $("#keyboard-upper-container").show();
-        } else {
-            $("#keyboard-lower-container").show();
-            $("#keyboard-upper-container").hide();
-        }
-        
-        if (e.type === 'keydown') {
-            handleHighlight(e, 'lightblue');
-        } else {
-            handleHighlight(e, '#f5f5f5');
-        }
+$(document).keyup(function() {
+    if(event.which == 16) {
+        lower.show();
+        upper.hide();
+    }   
+});
+
+$(document).keydown(function() {
+    if(event.which == 16) {
+        lower.hide();
+        upper.show();
+    }   
+});
+$('body').keypress(function(which) {
+    keyPress = String.fromCharCode(event.which);
+});
+
+$(document).keypress(function(event) {
+    $('#' + event.which).css('background-color', 'lightblue');
+        $(document).keyup(function() {
+            $('#' + event.which).css('background-color', '#f5f5f5');
         });
+});
+    $('#sentence').html((sentences[0].split('')));
+    $('body').on('keypress', function() {
+        b++;
+        move();
     });
-     
-function handleHighlight(e, color) {
-    var code = e.keyCode;
+});
 
-    if (!e.shiftKey && utils.isAlpha(code)) {
-        code = e.keyCode + 32;
+// function gameOver() {
+//     var lapse = milliseconds / 60000;
+//     var minutes = Math.round(lapse);
+//     var duration = (numOfWords / minutes - 2 * numWrong);
+//     var time = new Date() - start;
+//     var right = confirm('Speed: ' + duration + ' WPM.' + '\nErrors: ' + numWrong + '\nTry again???');
+//     if (yes == true){
+//         location.reload();
+//     } else {
+//         alert('Ok');
+//     }  
+// };
+function move() {
+    numMove++;
+    var sentChar = sentences[j].charAt(b);
+    $('#yellow-block').animate({left: "+=17px"}, 15);
+    $('#target-letter').text(sentChar);
+    if (sentChar == keyPress) {
+    $('#feedback').html(right);
+    } else if (numMove == 50){
+        gameOver();
     }
-
-    if (utils.isAlphanumeric(code)) {
-        $('#' + code).css('background-color', color);
+    else if (numMove >= sentences[j].length) {
+        j++;
+        $('#yellow-block').animate({left: "1em"}, 15);
+        $('#sentence').html((sentences[j]).split(''));
+        $('#feedback').html('');
+        numMove = 0;
+        b = -1;
+        return;
     } else {
-        $('span:contains('+ e.key +')').css('background-color', color);
-    }
-}
-var utils = {
-    isAlphanumeric: function(code) {
-        if ((code >= 65 && code <= 90) || (code >= 48 && code <= 57)) {
-            return true;
-        } else {
-            return false;
-    }
-},
-isNumber: function(code) {
-    if (code >= 48 && code <= 57){
-        return true;
-    } else {
-        return false;
-    }
-},
-isAlpha: function(code) {
-    if (code >= 65 && code <= 90) {
-        return true;
-    } else {
-        return false;
-         }
+        $('#feedback').html(wrong);
+        numWrong++;
     }
 };
-function mySentences() {
-    var sentenceDiv = $('#sentence');
-    sentenceDiv.empty();
-    sentenceDiv.append(sentence[sentenceCounter]);
-    sentenceCounter++;
-}
-mySentences();
 
-function handleResponse(e) {
-
-}
-
-    
 
 
 
